81b9d790747fa599d61b041da838dec461890015
Samuel Pimentel <53310044+samuelpszzz@users.noreply.github.com>
2022-05-25 02:57:05 -0300

    Primeros pasos y anotaciones

---
+++ Practice-1.py
@@ -0,0 +1,41 @@
+#Samuel Pimentel @samuelpszz
+a = "alo"
+a1 = " Mundo !."
+b = 2022
+c = -2022
+d = 20.22
+e = -20.22
+f = True 
+g = False
+
+a2 = type(a)
+print(a2)
+print("Para saber que tipo que dato almacena una variable escribimos: type(variable)")
+
+#suma
+a=100 
+b=100
+print(a+b)     
+c=a+(b+100)    
+print(c)    
+#resta
+c = 300
+d = c-(a+200) #300-300
+print(d)
+#division /
+a = 115
+b = a/5
+print("Division de 115 entre 5: "+ str(b))
+#division entera //
+b = 115//5
+print("Parte entera de 115 // 5: "+ str(b))
+#resto de la division % EJ: SABER SI UN N° ES PAR 
+a = 22
+a=a%2
+print("22 es multiplo de x2 su Resto es: " + str(a))
+
+a = "Alo"
+b = "Mundo"
+e = a+" "+b
+f = ". Soy Samuel !"
+print(e+f)
